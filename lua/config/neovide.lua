c1a7a69bb2e45ef2021b13633250336e92bd0ed4
zqr's macbook pro 2019 <zqr@zqrs-macbook.local>
2025-12-29 22:00:09 +0800

    feat: enable hidden files in snacks picker/explorer and refine keymaps

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -3,15 +3,15 @@ if vim.g.neovide then
   vim.g.neovide_opacity = 0.95
   vim.g.neovide_confirm_quit = true
   vim.g.neovide_remember_window_size = true
-  
+
   -- 2. 动画效果 (保持流畅感)
   vim.g.neovide_cursor_animation_length = 0.08
   vim.g.neovide_cursor_trail_size = 0.5
   vim.g.neovide_scroll_animation_length = 0.3
-  
+
   -- 3. 输入法优化 (macOS 必备)
   vim.g.neovide_input_ime = true
-  
+
   -- 4. 标题栏恢复默认 (由于 hidden 失效，我们保持默认以确保稳定)
   vim.o.title = true
   vim.o.titlestring = "%t - %{fnamemodify(getcwd(), ':t')}"
@@ -19,28 +19,33 @@ if vim.g.neovide then
   -- 5. 苹果用户核心快捷键 (Command 键映射)
   -- 映射 Command + 1-9 切换标签页 (类似 Chrome)
   for i = 1, 9 do
-    vim.keymap.set('n', '<D-' .. i .. '>', '<cmd>BufferLineGoToBuffer ' .. i .. '<cr>', { desc = "Switch to buffer " .. i })
+    vim.keymap.set(
+      "n",
+      "<D-" .. i .. ">",
+      "<cmd>BufferLineGoToBuffer " .. i .. "<cr>",
+      { desc = "Switch to buffer " .. i }
+    )
   end
 
   -- 映射 Command + s 保存
-  vim.keymap.set({'n', 'i', 'v'}, '<D-s>', '<cmd>w<cr>', { desc = "Save" })
+  vim.keymap.set({ "n", "i", "v" }, "<D-s>", "<cmd>w<cr>", { desc = "Save" })
   -- 映射 Command + w 关闭当前标签页
-  vim.keymap.set('n', '<D-w>', '<cmd>bd<cr>', { desc = "Close Buffer" })
-  
+  vim.keymap.set("n", "<D-w>", "<cmd>bd<cr>", { desc = "Close Buffer" })
+
   -- 映射 Command + z 撤销 (Undo)
-  vim.keymap.set({'n', 'i', 'v'}, '<D-z>', '<cmd>undo<cr>', { desc = "Undo" })
+  vim.keymap.set({ "n", "i", "v" }, "<D-z>", "<cmd>undo<cr>", { desc = "Undo" })
   -- 映射 Command + Shift + z 重做 (Redo)
-  vim.keymap.set({'n', 'i', 'v'}, '<D-S-z>', '<cmd>redo<cr>', { desc = "Redo" })
+  vim.keymap.set({ "n", "i", "v" }, "<D-S-z>", "<cmd>redo<cr>", { desc = "Redo" })
 
   -- 映射 Command + a 全选 (Select All)
-  vim.keymap.set({'n', 'i', 'v'}, '<D-a>', '<esc>ggVG', { desc = "Select All" })
+  vim.keymap.set({ "n", "i", "v" }, "<D-a>", "<esc>ggVG", { desc = "Select All" })
 
   -- 映射 Command + c 复制
-  vim.keymap.set('v', '<D-c>', '"+y', { desc = "Copy" })
+  vim.keymap.set("v", "<D-c>", '"+y', { desc = "Copy" })
   -- 映射 Command + v 粘贴
-  vim.keymap.set({'n', 'v'}, '<D-v>', '"+p', { desc = "Paste" })
-  vim.keymap.set('i', '<D-v>', '<C-r>+', { desc = "Paste" })
-  vim.keymap.set('c', '<D-v>', '<C-r>+', { desc = "Paste" })
+  vim.keymap.set({ "n", "v" }, "<D-v>", '"+p', { desc = "Paste" })
+  vim.keymap.set("i", "<D-v>", "<C-r>+", { desc = "Paste" })
+  vim.keymap.set("c", "<D-v>", "<C-r>+", { desc = "Paste" })
   -- 映射 Command + q 退出
-  vim.keymap.set({'n', 'i', 'v'}, '<D-q>', '<cmd>confirm qall<cr>', { desc = "Quit" })
+  vim.keymap.set({ "n", "i", "v" }, "<D-q>", "<cmd>confirm qall<cr>", { desc = "Quit" })
 end
